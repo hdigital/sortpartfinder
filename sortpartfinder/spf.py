41f685e78dd30424d715f8e35f89e52011525aea
Holger Döring <doering@uni-bremen.de>
2024-04-02 16:49:36 +0200

    Lint and format code with Ruff

--- sortpartfinder/spf.py
+++ sortpartfinder/spf.py
@@ -1,19 +1,18 @@
-'''
+"""
 Created on 03.01.2024
 
 @author: neumann
-'''
+"""
 import sqlite3
 
-if __name__ == '__main__':    
+if __name__ == "__main__":
     conn = sqlite3.connect("..\database\db.sqlite")
     cur = conn.cursor()
-    
+
     cur.execute("SELECT * FROM ROOMS")
-    
+
     for room in cur.fetchall():
         print(room)
-    
+
     cur.close()
     conn.close()
-
